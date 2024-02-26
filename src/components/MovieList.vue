ce661afb7f3c17ca34a75b4f55a561daa5cb1c0b
Axadyc <adamlmr62@gmail.com>
2024-02-26 09:21:00 +0100

    Initial commit

---
+++ src/components/MovieList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <data value="img">this.image</data>
+    </div>
+</template>
+<script>
+
+import axios from 'axios';
+import config from '..\config.json';
+
+export default {
+    name: 'MoviesList',
+    methods: {
+        getMovies() {
+        axios.get(config.url.movie_list, {
+            headers: {
+                'Authorization': `token ${config.token}`
+            }
+        })
+        .then(response => {
+            console.log(response.data);
+            image = config.photo_path
+            
+        })
+        .catch(error => {
+            console.log(error);
+        })
+        }
+    }
+}
+
+</script>
